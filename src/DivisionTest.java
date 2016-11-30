cd85469a462a46f76d0026ab3d542aac47a05b51
Micha Oberstein <benj.konto@t-online.de>
2016-11-30 19:53:46 +0100

    Add test 'differentPrecision'

--- src/DivisionTest.java
+++ src/DivisionTest.java
@@ -15,4 +15,13 @@ public class DivisionTest {
     {
         assertEquals(new Division("5", "0").divide(), "ERROR: Divide by zero");
     }
+
+    @Test
+    public void differentPrecisions()
+    {
+        assertEquals(new Division("1", "3", 0).divide(), "0");
+        assertEquals(new Division("1", "3", 1).divide(), "0.3");
+        assertEquals(new Division("1", "3", 2).divide(), "0.33");
+        assertEquals(new Division("1", "3", 3).divide(), "0.333");
+    }
 }
