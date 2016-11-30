4003ad5c402b10603f4ba6d3b3dca9ead9e76064
Micha Oberstein <benj.konto@t-online.de>
2016-11-30 20:10:32 +0100

    Add Test whitespace

--- src/DivisionTest.java
+++ src/DivisionTest.java
@@ -31,4 +31,18 @@ public class DivisionTest {
         assertEquals(new Division(null, "3", 3).divide(), "ERROR: NULL Value");
         assertEquals(new Division("3", null, 3).divide(), "ERROR: NULL Value");
     }
+
+    @Test
+    public void whitespace()
+    {
+        assertEquals(new Division(" ", "3", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("\t", "3", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("\r", "3", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("\n", "3", 3).divide(), "ERROR: Empty Value");
+        
+        assertEquals(new Division("3", " ", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("3", "\t", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("3", "\r", 3).divide(), "ERROR: Empty Value");
+        assertEquals(new Division("3", "\n", 3).divide(), "ERROR: Empty Value");
+    }
 }
